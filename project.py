7887ff965f7bb6360b259340e209ad2104f314c8
VitorCMatias <vitorcosta.matias@gmail.com>
2021-02-14 16:40:49 -0300

    Small refactoring
    Put some declaration at the top of the code.

--- project.py
+++ project.py
@@ -6,15 +6,12 @@ from hurry.filesize import alternative, size
 link = 'https://www.youtube.com/watch?v=Kee9Et2j7DA'  # input("Enter the link of YouTube video you want to download:  ")
 yt = YouTube(link)
 ys = yt.streams
+video_size = ys.first().filesize
+
 print("Title: ", yt.title)
 print("Number of views: ", yt.views)
 print("Length of video: ", time.strftime('%H:%M:%S', time.gmtime(yt.length)))
-
-video_size = ys.first().filesize
 print('File Size: ', size(video_size, system=alternative))
-
-# Getting the highest resolution possible
-
 
 # Starting download
 print("Downloading...")
