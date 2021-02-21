25143ea3ce864e5dffaaf7453812c31037f2747c
VitorCMatias <vitorcosta.matias@gmail.com>
2021-02-20 22:23:39 -0300

    Added a progress bar to download the video.
    Use the API pytube.cli iand the function on_progress

--- project.py
+++ project.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+
+from pytube.cli import on_progress
 from pytube import YouTube
 import time
-from progress.bar import Bar
 from hurry.filesize import alternative, size
 
 link = 'https://www.youtube.com/watch?v=Kee9Et2j7DA'  # input("Enter the link of YouTube video you want to download:  ")
-yt = YouTube(link)
+yt = YouTube(link, on_progress_callback=on_progress)
 ys = yt.streams
 video_size = ys.first().filesize
 
@@ -14,7 +16,7 @@ print("Length of video: ", time.strftime('%H:%M:%S', time.gmtime(yt.length)))
 print('File Size: ', size(video_size, system=alternative))
 
 # Starting download
-print("Downloading...")
+#print("Downloading...")
 ys.get_highest_resolution().download()
 print("Download completed!!")
 
