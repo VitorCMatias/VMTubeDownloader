eaae9d1c8bb906f75a6c6aec9080bdcda0791ca0
VitorCMatias <vitorcosta.matias@gmail.com>
2021-02-20 23:19:08 -0300

    extracted methods from the code
    Refactoring the code to separate its responsibilities. Added print_video_information, download_video and main.

--- project.py
+++ project.py
@@ -6,17 +6,29 @@ import time
 from hurry.filesize import alternative, size
 import sys
 
-link = sys.argv[1]
-yt = YouTube(link, on_progress_callback=on_progress)
-ys = yt.streams
-video_size = ys.first().filesize
 
-print("Title: ", yt.title)
-print("Number of views: ", yt.views)
-print("Length of video: ", time.strftime('%H:%M:%S', time.gmtime(yt.length)))
-print('File Size: ', size(video_size, system=alternative))
+def main():
+    link = sys.argv[1]
+    yt = YouTube(link, on_progress_callback=on_progress)
+    ys = yt.streams
+    video_size = size(ys.first().filesize, system=alternative)
 
-# Starting download
-#print("Downloading...")
-ys.get_highest_resolution().download()
-print("Download completed!!")
+    print_video_information(yt.title, yt.views, yt.length, video_size)
+    download_video(ys)
+
+
+def print_video_information(title, views, video_length, file_size):
+    print("Title: ", title)
+    print("Number of views: ", views)
+    print("Length of video: ", time.strftime('%H:%M:%S', time.gmtime(video_length)))
+    print('File Size: ', file_size)
+
+
+def download_video(youtube_stream):
+    # Starting download
+    youtube_stream.get_highest_resolution().download()
+    print("\nDownload completed!!")
+
+
+if __name__ == "__main__":
+    main()
