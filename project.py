2e9570c15f3a5e26709973995d69bc0607f0738a
VitorCMatias <vitorcosta.matias@gmail.com>
2021-02-20 22:49:37 -0300

    Added input py environment variables
    Import sys pack.

--- project.py
+++ project.py
@@ -4,8 +4,9 @@ from pytube.cli import on_progress
 from pytube import YouTube
 import time
 from hurry.filesize import alternative, size
+import sys
 
-link = 'https://www.youtube.com/watch?v=Kee9Et2j7DA'  # input("Enter the link of YouTube video you want to download:  ")
+link = sys.argv[1]
 yt = YouTube(link, on_progress_callback=on_progress)
 ys = yt.streams
 video_size = ys.first().filesize
@@ -19,5 +20,3 @@ print('File Size: ', size(video_size, system=alternative))
 #print("Downloading...")
 ys.get_highest_resolution().download()
 print("Download completed!!")
-
-
